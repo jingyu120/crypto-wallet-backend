e1abb0c2a1f756e7b83a1a4a33a5978291b1f19e
Justin <jingyu120@gmail.com>
2021-12-22 13:17:59 -0600

    add chat functionality

--- server/index.js
+++ server/index.js
@@ -7,20 +7,29 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "https://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id);
-    });
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
 server.listen(3001, () => {
-    console.log("Server running");
+  console.log("Server running");
 });
