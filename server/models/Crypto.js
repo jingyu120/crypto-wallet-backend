c2c80c1181662577f109e6a2a0c5be502453703f
Justin <jingyu120@gmail.com>
2022-01-03 13:24:37 -0600

    separte crypto from user schema

---
+++ server/models/Crypto.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const CryptoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  cost: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
+const CryptoModel = mongoose.model("cryptos", CryptoSchema);
+module.exports = CryptoModel;
