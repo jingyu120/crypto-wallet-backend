a0f07a55d6a99c65c93151dd25e5637eb64db855
Justin <jingyu120@gmail.com>
2022-01-03 13:22:47 -0600

    update users schema

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ const cors = require("cors");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
+const CryptoModel = require("./models/Crypto");
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
@@ -50,11 +51,57 @@ app.get("/getUsers", (req, res) => {
 
 app.post("/createUser", async (req, res) => {
   const user = req.body;
+  console.log(user);
   const newUser = new UserModel(user);
   await newUser.save();
 
   res.json(user);
 });
+
+app.get("/getCoins", (req, res) => {});
+
+app.post("/addCoin", async (req, res) => {
+  const { email, name, amount, cost } = req.body;
+  delete req.body.email;
+
+  try {
+    const user = await UserModel.findOne({ email });
+    const fetchedWallet = user.wallet.filter((u) => u.name === name);
+
+    if (fetchedWallet.length > 0) {
+      fetchedWallet.map((u) => {
+        u.amount += amount;
+        u.cost += cost;
+      });
+      await user.save();
+      console.log("updated coin");
+    } else {
+      const newCoin = new CryptoModel(req.body);
+      user.wallet.push(newCoin);
+      await user.save();
+      console.log("added new coin");
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+async function run() {
+  try {
+    const userEmail = "jingyu120@gmail.com";
+    const newCoin = new CryptoModel({ name: "LTC", amount: 1, cost: 500 });
+    const user = await UserModel.findOne({ email: userEmail });
+
+    console.log(user.wallet);
+    user.wallet = [];
+
+    await user.save();
+    console.log(user.wallet);
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+// run();
 
 server.listen(3001, () => {
   console.log("Server running");
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,18 +1,41 @@
 const mongoose = require("mongoose");
 
+const CryptoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  cost: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  age: {
-    type: Number,
-    required: true,
-  },
-  username: {
+  birthday: {
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    lowercase: true,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  wallet: [CryptoSchema],
 });
 
 const UserModel = mongoose.model("users", UserSchema);
