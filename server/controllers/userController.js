00d9a1e49c790bb06db4e12d7e37b84d59ae1b34
Justin <jingyu120@gmail.com>
2022-02-25 12:51:56 -0600

    refactor cryptoroutes to usercontroller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+import CryptoModel from "../models/Crypto";
+
 const UserModel = require("../models/Users");
 
 export const GetAllUsers = (req, res) => {
@@ -32,4 +34,108 @@ export const SignInGoogle = async (req, res) => {
       }
     }
   });
+};
+
+export const GetBalance = async (req, res) => {
+  try {
+    const email = req.params.email;
+    const data = await UserModel.findOne({ email });
+    res.status(200).json(data.balance);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+export const Deposit = async (req, res) => {
+  try {
+    const email = req.params.email;
+
+    const user = await UserModel.findOne({ email });
+    user.balance += Number(req.params.amount);
+    await user.save();
+    res.status(200).json(user.balance);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+export const Withdraw = async (req, res) => {
+  try {
+    const email = req.params.email;
+    const user = await UserModel.findOne({ email });
+    user.balance -= Number(req.params.amount);
+    await user.save();
+    res.status(200).json(user.balance);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+export const GetUserWallet = async (req, res) => {
+  try {
+    const email = req.query.email;
+    const data = await UserModel.findOne({ email });
+    res.status(200).json(data.wallet);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+//incomplete
+export const GetNetworth = async (req, res) => {
+  try {
+    const email = req.params.email;
+    const user = await UserModel.findOne({ email });
+    console.log(user.wallet);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+export const SellCoin = async (req, res) => {
+  const { name, amount, cost } = req.body;
+  const email = req.params.email;
+
+  try {
+    const user = await UserModel.findOne({ email });
+    const fetchedWallet = user.wallet.filter((u) => u.name === name);
+
+    if (fetchedWallet.length > 0) {
+      fetchedWallet.map((u) => {
+        u.amount -= amount;
+        u.cost -= cost;
+      });
+      user.balance -= cost;
+      await user.save();
+      res.status(200).json(user.balance);
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+export const BuyCoin = async (req, res) => {
+  const { name, amount, cost } = req.body;
+  const email = req.params.email;
+  try {
+    const user = await UserModel.findOne({ email });
+    const fetchedWallet = user.wallet.filter((u) => u.name === name);
+
+    if (fetchedWallet.length > 0) {
+      fetchedWallet.map((u) => {
+        u.amount += amount;
+        u.cost += cost;
+      });
+      user.balance -= cost;
+      await user.save();
+    } else {
+      const newCoin = new CryptoModel(req.body);
+      user.wallet.push(newCoin);
+      user.balance -= cost;
+      await user.save();
+    }
+    res.status(200).json(user.balance);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
--- server/routes/cryptoRoutes.js
+++ server/routes/cryptoRoutes.js
@@ -3,6 +3,13 @@ import {
   CreateNewUser,
   GetAllUsers,
   SignInGoogle,
+  GetBalance,
+  Deposit,
+  Withdraw,
+  GetUserWallet,
+  GetNetworth,
+  SellCoin,
+  BuyCoin,
 } from "../controllers/userController";
 const UserModel = require("../models/Users");
 const CryptoModel = require("../models/Crypto");
@@ -13,117 +20,18 @@ router.post("/createUser", CreateNewUser);
 
 router.post("/google_sign_in", SignInGoogle);
 
-router.get("/:email/balance", async (req, res) => {
-  try {
-    const email = req.params.email;
-    const data = await UserModel.findOne({ email });
-    res.json(data.balance);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+router.get("/:email/balance", GetBalance);
 
-router.post("/:email/depositCash/:amount", async (req, res) => {
-  try {
-    const email = req.params.email;
-    try {
-      const user = await UserModel.findOne({ email });
-      user.balance += Number(req.params.amount);
-      user.save();
-      res.json(user.balance);
-    } catch (error) {
-      console.log(error.message);
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+router.post("/:email/depositCash/:amount", Deposit);
 
-router.post("/:email/withdrawCash/:amount", async (req, res) => {
-  try {
-    const email = req.params.email;
-    try {
-      const user = await UserModel.findOne({ email });
-      user.balance -= Number(req.params.amount);
-      user.save();
-      res.json(user.balance);
-    } catch (error) {
-      console.log(error.message);
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+router.post("/:email/withdrawCash/:amount", Withdraw);
 
-router.get("/getCoins", async (req, res) => {
-  try {
-    const email = req.query.email;
-    const data = await UserModel.findOne({ email });
-    res.json(data.wallet);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+router.get("/getCoins", GetUserWallet);
 
-router.get("/:email/networth", async (req, res) => {
-  try {
-    const email = req.params.email;
-    const user = await UserModel.findOne({ email });
-    console.log(user.wallet);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+router.get("/:email/networth", GetNetworth);
 
-router.post("/:email/sellCoin", async (req, res) => {
-  const { name, amount, cost } = req.body;
-  const email = req.params.email;
+router.post("/:email/sellCoin", SellCoin);
 
-  try {
-    const user = await UserModel.findOne({ email });
-    const fetchedWallet = user.wallet.filter((u) => u.name === name);
-
-    if (fetchedWallet.length > 0) {
-      fetchedWallet.map((u) => {
-        u.amount -= amount;
-        u.cost -= cost;
-      });
-      user.balance -= cost;
-      await user.save();
-      res.json(user.balance);
-      console.log("updated coin");
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-
-router.post("/:email/addCoin", async (req, res) => {
-  const { name, amount, cost } = req.body;
-  const email = req.params.email;
-  try {
-    const user = await UserModel.findOne({ email });
-    const fetchedWallet = user.wallet.filter((u) => u.name === name);
-
-    if (fetchedWallet.length > 0) {
-      fetchedWallet.map((u) => {
-        u.amount += amount;
-        u.cost += cost;
-      });
-      user.balance -= cost;
-      await user.save();
-      console.log("updated coin");
-    } else {
-      const newCoin = new CryptoModel(req.body);
-      user.wallet.push(newCoin);
-      user.balance -= cost;
-      await user.save();
-      console.log("added new coin");
-    }
-    res.json(user.balance);
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+router.post("/:email/addCoin", BuyCoin);
 
 module.exports = router;
